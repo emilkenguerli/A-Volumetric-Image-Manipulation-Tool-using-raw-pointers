b48f510ed31cdac3e8764b4ec8a4d7bb0fe5fd8d
Emil <emil@gmail.com>
2019-03-19 00:46:22 +0200

    Completed Version

---
+++ Driver.cpp
@@ -0,0 +1,199 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "tool.h"
+
+using namespace std;
+
+static void show_usage(std::string name)
+{
+    std::cerr << "Usage: " << name << " <option(s)>\n"
+              << "Options:\n"
+              << "\t-h,--help\t\tShow this help message\n"
+              << "\t-d,--difference \tSpecify difference map between\n"
+              << "\t\t\t\timages i and j, and write out to file.\n"
+              << "\t\t\t\tEnter numbers i and j followed by file name.\n"
+              << "\t-x,--extract \t\tExtract and write the slice with\n"
+              << "\t\t\t\tnumber i and write out to file.\n"
+              << "\t\t\t\tEnter number i and file name.\n"
+              << std::endl;
+}
+
+static int parse_args(int argc, char* argv[], int& option_chosen, 
+    std::string& image_base, int& number1, int& number2, std::string& output_file)
+{
+    std::string difference_args;
+    std::string extract_args;
+    std::string dif_output_file;
+    std::string slice;
+    std::string ext_output_file;
+    int first_image_number;
+    int second_image_number;
+    int slice_number;
+    option_chosen = 0;
+
+
+    if (argc < 2 || argc > 6 || argc == 3 || argc == 4)
+    {
+        show_usage(argv[0]);
+        return 1;
+    }
+
+    if (argc == 2) 
+    {
+        option_chosen = 1; // do your default things here
+        image_base = argv[1]; // Get imageBase - the prefix for the file sequence
+        if (image_base[0] == '-') {
+            cout << "Invalid image_base file name \n" << image_base << endl; 
+            show_usage(argv[0]);        
+        }
+    }
+    int i;
+    for (option_chosen == 0, i = 2; i < argc; ++i) 
+    {
+        if (argc == 2) option_chosen = 1; // do your default things here
+        image_base = argv[1]; // Get imageBase - the prefix for the file sequence
+        if (image_base[0] == '-') {
+            cout << "Invalid image_base file name \n" << image_base << endl; 
+            show_usage(argv[0]);        
+        }
+
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help"))  
+        {
+            show_usage(argv[0]);
+            return 0;
+        } 
+        else if ((arg == "-d") || (arg == "--difference")) 
+        {
+            if (i + 3 < argc && argc < 7) 
+            { // Make sure we have enough difference argiments
+                difference_args = argv[i++]; 
+                std::istringstream convert1(argv[i++]); 
+                if ( !(convert1 >> number1) )  
+                {
+                    number1 = 0; 
+                    std::cerr << "non-numeric first image number\n" << std::endl;
+                    show_usage(argv[0]);
+                    return 1;
+                } 
+ 
+                std::istringstream convert2(argv[i++]); 
+                if ( !(convert2 >> number2) ) 
+                {
+                    number2 = 0; 
+                    std::cerr << "non-numeric second image number\n" << std::endl;
+                    show_usage(argv[0]);
+                    return 1;
+                } 
+
+                output_file = argv[i++];
+                option_chosen = 2;
+            } 
+            else 
+            { // Not enough arguments to the difference option.
+                std::cerr << "-d option requires 3 arguments." << std::endl;
+                show_usage(argv[0]);
+                return 1;
+            }  
+        } 
+        else if ((arg == "-x") || (arg == "--extract")) 
+        {
+            if (i + 2 < argc && argc < 6) 
+            { // Make sure we have enough extract argiments
+                extract_args = argv[i++];
+                slice = argv[i++];
+                std::istringstream convert(slice); 
+                if ( !(convert >> number1) ) 
+                {
+                    number1 = 0; 
+                    std::cerr << "non-numeric slice number\n" << std::endl;
+                    show_usage(argv[0]);
+                    return 1;
+                } 
+
+                output_file = argv[i++];
+                option_chosen = 3;
+            } 
+            else 
+            { // Not enough arguments to the extract option.
+                std::cerr << "-x option requires 2 arguments." << std::endl;
+                show_usage(argv[0]);
+                return 1;
+            }  
+        } 
+        else 
+        {
+             std::cerr << "No Option chosen" << std::endl;
+             show_usage(argv[0]);
+             return 1;
+        }
+    }
+}
+
+using namespace KNGEMI002;
+int main(int argc, char* argv[])
+{
+    int option = 0;
+    std::string image = "";
+    std::string output_file = "";
+    int number1 = 0;
+    int number2 = 0;
+ 
+    std::string difference_output_file;
+    std::string exttract_output_file;
+    int first_image_number;
+    int second_image_number;
+    int slice_number;
+
+	KNGEMI002::VolImage vi;
+
+    parse_args(argc, argv, option, image, number1, number2, output_file);
+    cout << "option = " << option << endl;
+	cout << "Image_base = " << image << endl;
+	
+    if (option == 1) // no arguments passed
+    {   
+		 cout << "Number of images: " << KNGEMI002::getNumberofImages(image) << std::endl;
+		 cout << "Number of bytes required: " << vi.volImageSize() << std::endl;
+		 if(vi.readImages(image)){
+			cout << "Images read: True\n";
+		 }
+		 else{
+			cout << "Images read: False\n";
+		 }
+		 
+    }
+    else if (option == 2) // -d chosen
+    {
+        first_image_number = number1;
+        second_image_number = number2;
+        difference_output_file = output_file;
+        cout << "Image_base = " << image << endl;
+        cout << "First image number: " << first_image_number << endl;
+        cout << "Second image number: " << second_image_number << endl;
+        cout << "Difference output filename: " << output_file << endl;
+
+		KNGEMI002::getNumberofImages(image);
+		vi.diffmap(first_image_number, second_image_number, output_file);
+
+    } 
+    else if (option == 3) // -x chosen
+    {
+        slice_number = number1;
+        cout << "Image_base = " << image << endl;
+        cout << "Slice number: " << slice_number << endl;
+        cout << "Extract output filename: " << output_file << endl;
+
+		KNGEMI002::getNumberofImages(image);
+		vi.extract(slice_number, output_file);
+    } 
+    else
+    {
+        cout << "No option chosen" << endl;
+    }
+
+    return 0;
+}
---
+++ tool.cpp
@@ -0,0 +1,301 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "tool.h"
+
+using namespace std;
+
+int imageWidth;
+int imageHeight;
+std::string filename;
+int numOfImages;
+
+KNGEMI002::VolImage::VolImage(){
+	width = 0;
+	height = 0;
+}
+	
+KNGEMI002::VolImage::~VolImage(){
+            vector<unsigned char**>::iterator it;
+            for(it=slices.begin();it!=slices.end();it++)
+            {
+                delete **it;            
+            }
+            slices.clear();
+}
+
+bool KNGEMI002::VolImage::readImages(std::string baseName){
+        for (int j = 0; j < numOfImages; j++)
+        {
+            ifstream image_base_file;
+            unsigned char ** bytes = new unsigned char*[imageWidth]; //raw file goes here
+            // 429 col, 303 row
+            for  (int l = 0; l < imageWidth; l++)
+            {
+                bytes[l] = new unsigned char[imageHeight];
+            }
+            string s = to_string(j);
+            string ext = ".raw";
+            string file_name = baseName+s+ext;
+            image_base_file.open(file_name,std::ios_base::binary);
+            if (!image_base_file) 
+            {
+                std::cerr << "Could not open the file!" << std::endl;
+            }
+            else
+            {
+                int byte_count = 0;
+                for (int k = 0; k < imageWidth; k++)
+                {
+                    for  (int m = 0; m < imageHeight; m++)
+                    {
+                        unsigned char byte;
+
+                        image_base_file >> byte; //byte by byte
+
+                        if (image_base_file.fail())
+                        {
+                            // end of file
+                            break;
+                        }
+
+                        bytes[k][m] = byte;
+                        byte_count++;
+                     }
+                 }
+            }
+            slices.push_back(bytes); //pushing into the vector of slices
+        }
+		return true;
+}
+
+void KNGEMI002::VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix){
+	    int j = 0;
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 0) j = sliceI;
+            if (i == 1) j = sliceJ;
+            ifstream image_base_file;
+            // allocate memory
+		cout << "imageWidth = " << imageWidth << endl; 
+		cout << "imageHeight = " << imageHeight << endl; 
+            unsigned char ** bytes = new unsigned char*[imageWidth]; //raw file goes here
+            for  (int l = 0; l < imageWidth; l++)
+            {
+                bytes[l] = new unsigned char[imageHeight];
+            }
+            string s = to_string(j);
+            string ext = ".raw";
+            string file_name = "MRI"+s+ext;
+            cout << file_name << endl;
+            image_base_file.open(file_name,std::ios_base::binary);
+            if (!image_base_file) 
+            {
+                std::cerr << "Could not open the file!" << std::endl;
+            }
+            else
+            {
+                int byte_count = 0;
+                for (int k = 0; k < imageWidth; k++)
+                {
+                    for  (int m = 0; m < imageHeight; m++)
+                    {
+                        unsigned char byte;
+
+                        image_base_file >> byte; //byte by byte
+
+                        if (image_base_file.fail())
+                        {
+                            // end of file
+                            break;
+                        }
+
+                        bytes[k][m] = byte;
+                        byte_count++;
+                     }
+                 }
+                 cout << "Number of bytes = " << byte_count << endl;
+            }
+            slices.push_back(bytes); //pushing into the vector of slices
+        }
+        std::fstream output_file_stream;
+		output_prefix = output_prefix + ".raw";
+        output_file_stream.open(output_prefix, std::ios::app | std::ios::binary);
+  
+        unsigned char output_pixel;
+        int number_of_pixels = 0;
+		for (int p = 0; p < imageWidth; p++)
+        {
+            for  (int m = 0; m < imageHeight; m++)
+            {
+                output_pixel = (unsigned char)(abs((float)slices.at(0)[p][m] - (float)slices.at(1)[p][m])/2);
+                output_file_stream.write(reinterpret_cast<char*>(&output_pixel), sizeof(output_pixel));
+                number_of_pixels++;
+          }
+        }
+        output_file_stream.close();
+        cout << "Number of pixels = " << number_of_pixels << endl;    
+    
+
+}
+
+void KNGEMI002::VolImage::extract(int sliceId, std::string output_prefix){
+			ifstream image_base_file;
+            // allocate memory
+            unsigned char ** bytes = new unsigned char*[imageWidth]; //raw file goes here
+            for  (int l = 0; l < imageWidth; l++)
+            {
+                bytes[l] = new unsigned char[imageHeight];
+            }
+            string s = to_string(sliceId);
+            string ext = ".raw";
+            string file_name = "MRI"+s+ext;
+            image_base_file.open(file_name,std::ios_base::binary);
+			cout << "imageWidth = " << imageWidth << endl;
+			cout << "imageHeight = " << imageHeight << endl;
+
+            if (!image_base_file) 
+            {
+                std::cerr << "Could not open the file!" << std::endl;
+            }
+            else
+            {
+                int byte_count = 0;
+                for (int k = 0; k < imageWidth; k++)
+                {
+                    for  (int m = 0; m < imageHeight; m++)
+                    {
+                        unsigned char byte;
+
+                        image_base_file >> byte; //byte by byte
+
+                        if (image_base_file.fail())
+                        {
+                            // end of file
+                            break;
+                        }
+
+                        bytes[k][m] = byte;
+                        byte_count++;
+                     }
+                 }
+            }
+        slices.push_back(bytes); //pushing into the vector of slices
+
+        file_name = output_prefix+".dat";
+        ofstream header_file_stream (file_name);
+        if (header_file_stream.is_open())
+        {
+            header_file_stream << imageWidth << ", " << imageHeight << ", " << numOfImages << std::endl;
+            header_file_stream.close();
+        }
+        else cout << "Unable to open file";
+
+        std::fstream output_file_stream;
+		output_prefix = output_prefix +".raw";
+        output_file_stream.open(output_prefix, std::ios::app | std::ios::binary);
+  
+        unsigned char output_byte;
+        int number_of_bytes = 0;
+
+        for (int k = 0; k < imageWidth; k++)
+        {
+            for  (int m = 0; m < imageHeight; m++)
+            {
+                output_byte = (unsigned char)slices.at(0)[k][m];
+                output_file_stream.write(reinterpret_cast<char*>(&output_byte), sizeof(output_byte));
+                number_of_bytes++;
+            }
+        }
+        output_file_stream.close();
+        cout << "Number of bytes written= " << number_of_bytes << endl;  
+
+}
+
+int KNGEMI002::VolImage::volImageSize(void){
+    int size;
+	int byte_count = 0;
+	for (int j = 0; j < numOfImages; j++)
+        {
+            ifstream image_base_file;
+            string s = to_string(j);
+            string ext = ".raw";
+            string file_name = filename+s+ext;
+            image_base_file.open(file_name,std::ios_base::binary);
+            if (!image_base_file) 
+            {
+                std::cerr << "Could not open the file!" << std::endl;
+            }
+            else
+            {               
+                for (int k = 0; k < imageWidth; k++)
+                {
+                    for  (int m = 0; m < imageHeight; m++)
+                    {
+                        unsigned char byte;
+
+                        image_base_file >> byte; //byte by byte
+
+                        if (image_base_file.fail())
+                        {
+                            // end of file
+                            break;
+                        }
+
+                        byte_count++;
+                     }
+                 }
+            }
+        }
+
+	int bytes = sizeof(int);
+	size = byte_count + (bytes*numOfImages);
+
+	return size;
+}
+
+int KNGEMI002::getNumberofImages(std::string s){
+	std::ifstream file;
+	std::string fileName = s + ".data";
+	filename = s;
+    file.open(fileName); 
+
+	if(!file){ 
+		cout << "File is not a .data file but a .dat" << endl;
+		fileName = s + ".dat";
+		file.open(fileName);
+	}
+
+    int data=0;
+	std::vector<std::string> v;
+	std::vector<std::string> v2;
+	std::string line;
+
+	if(!file){ 
+		cout << "Couldn't open file" << endl; 
+	}
+	else{		
+		while(std::getline(file, line)){
+			v.push_back(line);			
+		}
+		std::string word;
+		std::istringstream iss(v[0]);
+
+		while(std::getline(iss, word, ' ')){
+			v2.push_back(word);				
+		}
+	}
+
+	imageWidth = stoi(v2[0]);
+	imageHeight = stoi(v2[1]);
+	int size = stoi(v2[2]);
+    file.close();
+	numOfImages = size;
+
+	return size;
+}
